4179e8f18c8c04aa1689b2840da6a01bc1968682
Alex Market <mercadoalex@gmail.com>
2022-12-02 13:32:00 -0600

    Primer commit royecto de microservicios, docker y kubernetes

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('post/:id/comments', (req,res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+
+});
+app.post('post/:id/comments', (req,res) => {
+   const commentId = randomBytes(4).toString('hex');
+   const { content } = req.body;    
+   const comments = commentsByPostId[req.params.id] || [];
+   comments.push({id:commentId, content}) ;
+   
+   commentsBtPostId[req.params.id] = comments;
+   res.status(201).send(comments);
+});
+app.listen(4001, () => {
+    console.log('Escuchando en el 4001');
+});
